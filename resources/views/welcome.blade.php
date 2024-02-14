8f3359f08081f02a7533b872ba5bfc7c365fd115
Harsha Nawana <harshanawana@gmail.com>
2024-02-14 10:50:07 +0530

    Sectioning
    Divided page into sections and added some css to set section sizes.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,20 +11,48 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <!-- Styles -->
+        <link rel="stylesheet" href="/css/main.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
     </head>
-    <body class="antialiased">
-        <nav class="navbar bg-body-tertiary">
-            <div class="container">
-                <a class="navbar-brand" href="#">
-                <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24">
-                </a>
-            </div>
-        </nav>
-        <section class="header">
-            <h1>Heading</h1>
+    <body>
+
+        <!-- Home -->
+        <section class="home">
+            <section class="nav-bar">
+                <h1>Nav Bar</h1>
+            </section>
+            <h1>Home Section</h1>
         </section>
+
+        <hr class="solid">
+
+        <!-- Events -->
+        <section class="events">
+            <h1>Event Section</h1>
+        </section>
+
+        <hr class="solid">
+
+        <!-- News -->
+        <section class="news">
+            <h1>News Section</h1>
+        </section>
+
+        <hr class="solid">
+
+        <!-- Community -->
+        <section class="community">
+            <h1>Community Section</h1>
+        </section>
+
+        <hr class="solid">
         
+        <footer>
+            <h1>footer</h1>
+        </footer>
     </body>
 
     <!-- Scripts -->
